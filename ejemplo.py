84839912e307a2aef489fd2dbceb39243a68fd37
andmansim <91722324+andmansim@users.noreply.github.com>
2023-03-13 15:45:44 +0100

    importamos

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,12 @@
 #Explicación + capturas de lo que nos sale + código 
+#Importamos librerias
+from urllib.parse import*
+from contextlib import closing
+from sys import*
+from bs4 import*
+from os import sep
+from timeit import *
+from asyncio
+from aiohttp import *
+from functools import partial
+import html.parser
