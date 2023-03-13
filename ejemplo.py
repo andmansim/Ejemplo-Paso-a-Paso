0b83afe3dd2221981a7ac531e19621428eb7da57
andmansim <91722324+andmansim@users.noreply.github.com>
2023-03-13 15:51:26 +0100

    contenido de la uri

--- ejemplo.py
+++ ejemplo.py
@@ -6,7 +6,22 @@ from sys import*
 from bs4 import*
 from os import sep
 from timeit import *
-from asyncio
+import asyncio
 from aiohttp import *
 from functools import partial
 import html.parser
+
+def wget(uri):
+    parsed = urlparse(uri)
+    with closing (HTTPConnection(parsed.netloc)) as conn:
+        path = parsed.path
+        if parsed.query:
+            path += '¿?' + parsed.query
+        conn.request('GET', path)
+        response = conn.getresponse()
+        if response.status != 200:
+            print(response.reason, file = stderr)
+            return
+        print('Respuesta correcta')
+        return response.read()
+            
